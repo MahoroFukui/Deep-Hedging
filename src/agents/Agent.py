e0c1b44b21aa33d3997a9474de2042ffd8a9b57a
MahoroFukui <mahoroba3141@gmail.com>
2026-01-08 02:10:30 +0900

    Update Agent.py

--- src/agents/Agent.py
+++ src/agents/Agent.py
@@ -66,7 +66,7 @@ class Agent(torch.nn.Module, ABC):
         return self.forward(state)
 
     # returns the final p&l
-    def compute_portfolio(self, hedge_paths, logging = False) -> torch.Tensor:
+    def compute_portfolio(self, hedge_paths, logging = True) -> torch.Tensor:
         # number of time steps
         P, T, N = hedge_paths.shape
 
@@ -229,7 +229,7 @@ class Agent(torch.nn.Module, ABC):
         claim_payoff = contingent_claim.payoff(claim_path).to(self.device) # P x 1
 
         if self.criterion.__class__.__name__ == "CRRA":
-            portfolio_value, wealth_path = self.compute_portfolio_if_CRRA(hedge_paths, logging=False, initial_wealth=1.0)
+            portfolio_value, wealth_path = self.compute_portfolio_if_CRRA(hedge_paths, logging=True, initial_wealth=1.0)
         else:
             portfolio_value = self.compute_portfolio(hedge_paths, logging)
 
