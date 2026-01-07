de37e0b31c9dfce55033ecb9d02c29d84cd29293
MahoroFukui <mahoroba3141@gmail.com>
2026-01-08 08:03:03 +0900

    Update RecurrentAgent.py

--- src/agents/RecurrentAgent.py
+++ src/agents/RecurrentAgent.py
@@ -17,7 +17,7 @@ class RecurrentAgent(SimpleAgent):
 
         simple_features = super().feature_transform(state) # (P, N+1)
 
-        current_cash_account = state[1][:, -1].unsqueeze(1)
+        current_cash_account = state[1][:, -1]
         
         current_positions = state[2][:, -1] # (P, N)
         q_batch = state[4]
