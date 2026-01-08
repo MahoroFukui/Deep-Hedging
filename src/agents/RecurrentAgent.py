81e24d47fbb1d01f5c9d5a0e5d95fd0d4e2ae5ee
MahoroFukui <mahoroba3141@gmail.com>
2026-01-08 09:02:42 +0900

    Update RecurrentAgent.py

--- src/agents/RecurrentAgent.py
+++ src/agents/RecurrentAgent.py
@@ -17,23 +17,12 @@ class RecurrentAgent(SimpleAgent):
 
         simple_features = super().feature_transform(state) # (P, N+1)
 
-        P, t, N = state[0].shape
-        T = state[3]
-
-        times = torch.ones(P, 1, device=self.device) * (T-t) # (P, 1)
-
-        last_prices = state[0][:, -1, :] # (P, N)
-        # log prices
-        log_prices = torch.log(last_prices) # (P, N)
-
-        times = torch.ones(P, 1, device=self.device) * (T-t) # (P, 1)
-
         current_cash_account = state[1][:, -1]
        
         current_positions = state[2][:, -1] # (P, N)
-        q_batch = state[4][:, -1]
+        #q_batch = state[4][:, -1]
 
-        features = torch.cat([log_prices, times, current_cash_account, current_positions, q_batch], dim=1) # (P, 3N+2)
+        features = torch.cat([simple_features, current_cash_account, current_positions, q_batch], dim=1) # (P, 3N+2)
         
         #outdated: features = torch.cat([simple_features, current_positions], dim=1)
         
