167c069deb94c5923544efc7fa0153b052d09a2d
MahoroFukui <mahoroba3141@gmail.com>
2026-01-08 09:14:35 +0900

    Update RecurrentAgent.py

--- src/agents/RecurrentAgent.py
+++ src/agents/RecurrentAgent.py
@@ -17,10 +17,12 @@ class RecurrentAgent(SimpleAgent):
 
         simple_features = super().feature_transform(state) # (P, N+1)
 
-        current_cash_account = state[1]
-       
+        if
+        current_cash_account = state[1][:, -1]
+        q_batch = state[4][:, -1]
+        
         current_positions = state[2][:, -1] # (P, N)
-        #q_batch = state[4][:, -1]
+        
 
         features = torch.cat([simple_features, current_cash_account, current_positions, current_positions], dim=1) # (P, 3N+2)
         
