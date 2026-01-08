63eab20aae4be7c01c988ff3134a5147aa654c8b
MahoroFukui <mahoroba3141@gmail.com>
2026-01-08 15:45:40 +0900

    Update Agent.py

--- src/agents/Agent.py
+++ src/agents/Agent.py
@@ -390,7 +390,7 @@ class Agent(torch.nn.Module, ABC):
                 profit, wealth_path = self.pl(contingent_claim, current_batch_size, T, False, self.q)
                 EU_with_liability = self.criterion(profit)
                 loss_before_other_penalties = self.crra_ruin_penalized_loss(
-                    terminal_wealth=profit, wealth_path=wealth_path,lambda_ruin=50.0, tau=1e-2, p=1)
+                    terminal_wealth=profit, wealth_path=wealth_path,lambda_ruin=1.0, tau=1e-2, p=1)
                 low  = F.softplus((q_min - self.q) / 1e-2)
                 high = F.softplus((self.q - q_max) / 1e-2)
                 penalty_q = low**2 + high**2
