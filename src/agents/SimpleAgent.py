721f1db6f552664787e1f00a1b4bb970639fa9a0
MahoroFukui <mahoroba3141@gmail.com>
2026-01-09 19:15:14 +0900

    Update SimpleAgent.py

--- src/agents/SimpleAgent.py
+++ src/agents/SimpleAgent.py
@@ -21,7 +21,7 @@ class SimpleAgent(Agent):
         self.N = len(hedging_instruments)
         network_input_dim = self.input_dim()
 
-        super().__init__(criterion, cost_function, hedging_instruments, interest_rate, lr, pref_gpu)
+        super().__init__(criterion, cost_function, hedging_instruments, interest_rate, pref_gpu)
         self.q = torch.nn.Parameter(torch.tensor(q, dtype=torch.float32))
         self.network = torch.nn.Sequential(
         OrderedDict([
